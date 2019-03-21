16048b77774ec89629c23b7001e3cb96373a6df3
Mr <mickaelmendes50@gmail.com>
2019-03-21 20:24:31 -0300

    teste

---
+++ aula-02/decimal-binario.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main () {
+    int numeroDecimal, numeroBinario;
+
+    printf("Número decimal: \n");
+    scanf("d", &numeroDecimal);
+    numneroBinario = (numeroDecimal%2);
+}

f440de8fec3a7155e6bbc8d6bc868bccaf38b750
Mr Mesquita <mickaelmendes50@gmail.com>
2019-03-21 21:36:35 -0300

    c: Segundo método para Incremeto

--- aula-02/incremento.c
+++ aula-02/incremento.c
@@ -8,3 +8,16 @@ main () {
     S = N + 1;
     printf("O antecessor é %d e o sucessor é %d", A, S);
 }
+
+// Segundo método
+
+include <stdio.h>
+
+main () {
+    int N;
+    printf("Informe um número");
+    scanf("%d", &N);
+    A = N - 1;
+    S = N + 1;
+    printf("O antecessor é %d e o sucessor é %d", N--, N++);
+}

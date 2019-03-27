c72df1e0b604a56a5a9399306f32735a8018b382
Mr <mickaelmendes50@gmail.com>
2019-03-26 21:30:11 -0300

    aula-03: Third exercise

--- aula-03/aula-03.c
+++ aula-03/aula-03.c
@@ -15,7 +15,6 @@ main () {
 
     }
 }
-*/
 
 main () {
     int n1, n2;
@@ -29,5 +28,21 @@ main () {
     }
     else {
         printf("os números em ordem crescente são %d e %d", n2, n1);
+    }
+*/
+main () {
+    float n1, n2, media;
+
+    printf("informe suas notas \n");
+    scanf("%f %f", &n1, &n2);
+
+    media = (n1 + n2) / 2;
+
+    printf("Sua média é %.2f \n", media);
+    if (media >= 6) {
+        printf("Você foi APROVADO");
+    }
+    else {
+        printf("Você foi REPROVADO");
     }
 }

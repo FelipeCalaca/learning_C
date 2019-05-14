56d5809779aa703431ef2c015061451899fd76f0
Mr Mesquita <mickaelmendes50@gmail.com>
2019-05-14 20:20:02 -0300

    odenação: Do it better

--- aulas/aula-10/ordenação.c
+++ aulas/aula-10/ordenação.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    int num[10], j;
-    
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &num[i]);
+	int main() {
+	    int num[10], j;
 
-        for (int c = 0; c < 10; c++) {
-            if (num[c] < num[i]) {
-                j = num[i];
-                num[i] = num[c];
-                num[c] = j;
-            }
-        }
-    }
+	    for (int i = 0; i < 10; i++) {
+	        scanf("%d", &num[i]);
 
-    for (int p = 0; p < 10; p++) {
-        printf("%d ", num[p]);
-    }
-    return 0;
-}
+	        for (int c = 0; c < 9; c++) {
+	            if (num[c] > num[i]) {
+	                j = num[c];
+	                num[c] = num[i];
+	                num[i] = j;
+	            }
+	        }
+	    }
+
+	    for (int p = 0; p < 9; p++) {
+	        printf("%d ", num[p]);
+	    }
+	    return 0;
+	}
